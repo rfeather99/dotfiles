a161ee79a701f01caf284da6210eab1aa457f8ef
@rfeather99 <rfeather99@yahoo.co.jp>
2019-07-28 22:20:46 +0900

    :+1: スクリプト修正

--- link.sh
+++ link.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 script_dir=$(cd $(dirname $BASH_SOURCE); pwd)
 
-ln -sf ${script_dir}/.bash_profile ~/.bash_profile
-ln -sf ${script_dir}/.zshrc ~/.zshrc
-ln -sf ${script_dir}/.tmux.conf ~/.tmux.conf
-ln -sf ${script_dir}/.vimrc ~/.vimrc
-ln -sf ${script_dir}/.cache/dein/dein.toml ~/.cache/dein/dein.toml
-ln -sf ${script_dir}/.cache/dein/dein_lazy.toml ~/.cache/dein/dein_lazy.toml
+ln -sf ${script_dir}/.zshrc       ~/.zshrc
+ln -sf ${script_dir}/.tmux.conf   ~/.tmux.conf
+ln -sf ${script_dir}/.config/nvim ~/.config/nvim
 
